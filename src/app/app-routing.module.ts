1d50b4b5be9e442f30b51b69df582bb53f52e662
Babatunde Lamidi <odunayohlamidi@gmail.com>
2020-01-16 19:44:47 +0100

    ⚡️ chore: set routeroutlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ShortlyComponent } from './shortly/shortly.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/shortly', pathMatch: 'full' },
+  { path: 'shortly', component: ShortlyComponent }
+];
 
 @NgModule({
   declarations: [],
   imports: [
+    RouterModule.forRoot(routes),
     CommonModule
-  ]
+  ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }

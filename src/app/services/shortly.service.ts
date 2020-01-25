59afacdce16a242dc4859ac25d80f7da542cd0ab
Babatunde Lamidi <odunayohlamidi@gmail.com>
2020-01-25 21:04:06 +0100

    ⚡️ chore: post and get

--- src/app/services/shortly.service.ts
+++ src/app/services/shortly.service.ts
@@ -1,12 +1,36 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShortlyService {
 
+  private post = 'https://news.ycombinator.com';
+
   constructor (private http: HttpClient) { }
+
+
+  postURL (URL: string) {
+    return this.http.post(this.post, URL).pipe(
+      map((response) => {
+        if (response) response
+      }, (error) => error)
+    )
+  }
+
+  getURL (URL: string) {
+    return this.http.get(`${this.post}/${URL}`).pipe(
+      map((response) => {
+        if (response) response
+      }, (error) => error)
+    )
+  }
+
+
+
 
 
 }
